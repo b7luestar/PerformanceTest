87bad312fad4d7847b0169df178d7654761012c1
Lan Xin <xin.lan@titansoft.com.sg>
2024-09-12 15:55:03 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+import { ref } from 'vue';
+const toggle = ref(false);
+
+const toggleTimerComponent = () => {
+  toggle.value = !toggle.value;
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="btn btn-primary" @click="toggleTimerComponent">{{ toggle? "Delete": "Create" }} Timer component</div>
+  </div>
+  <HelloWorld v-if="toggle"/>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const timer = ref(0);
+const stringArray = ref([] as string[]);
+const counter = ref(0);
+
+const createTimer = () => {
+  timer.value = setInterval(() => {
+    console.log('Timer running!');
+    for (let i = 0; i < 500; i++) {
+      stringArray.value.push('Timer running: ' + counter.value++);
+    }
+  }, 500);
+}
+const clearTimers = () => {
+  clearInterval(timer.value);
+}
+
+onMounted(() => {
+  createTimer();
+});
+
+
+</script>
+
+<template>
+  <div class="mt-3">
+    <div class="btn m-2 btn-primary" @click="clearTimers">Clear Timers</div>
+  </div> 
+</template>
